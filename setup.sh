ad8469b122a8f7c0f2916c8ebb6dd5115bea1b86
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-04-21 23:32:10 -0400

    actually

--- setup.sh
+++ setup.sh
@@ -3,8 +3,12 @@
 cd TeamCode/src/main/
 
 mkdir java
+cd java
 mkdir org
+cd org
 mkdir firstinspires
+cd firstinspires
 mkdir ftc
+cd ftc
 
 git clone git@github.com:Fission310/freight-frenzy.git teamcode
